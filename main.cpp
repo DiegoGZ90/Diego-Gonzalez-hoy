3f8a873589c04dcb68d5c9b0929723ba9c307c16
DiegoGZ90 <116232459+DiegoGZ90@users.noreply.github.com>
2023-03-25 20:55:41 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,118 @@
+#include <iostream>}
+
+using namespace std;
+
+int main() {
+/*
+    int edad;
+    cout<<"Cual es tu edad: ";
+    cin>>edad;
+
+
+    if(edad >= 18){
+        cout<<"Eres mayor de edad";
+        cin>>edad;
+
+    }
+    else{
+        cout<<"Es Menor de edad: ";
+
+
+    }
+    return 0;
+}*/
+/*
+int c1,c2;
+
+    cout<<"Escriba un numero: ";
+    cin>>c1;
+    cout<<"Escriba otro numero: ";
+    cin>>c2;
+
+
+    if(c1>c2){
+        cout<<"El Numero es Mayor =>" <<c1;
+
+
+    }
+    else{
+        cout<<"Es Numero es menor: ";
+        cin>>c2;
+
+      return 0;
+    }
+    }
+int n;
+cout <<"Digite un numero entero: "; cin>>n;
+
+if(n % 2 ==0 ){
+    cout <<n<<" es par"<<endl;
+}else if(n% 2 ==!0){
+        cout<<n<<"es impar"<<endl;
+        return 0;
+}*/
+/*int n, f=1, m=1;
+
+    cout<<"Digite un numero: "; cin>>n;
+while(m<=n){
+    f *=m;
+    m++;
+}
+
+cout<<"El factorial de "<<n<<" es: "<<f<<endl;
+
+
+return 0;
+}*/
+
+    /*   int n;
+       cout<<"Numero mayor que de 10 y menor que 20: " << endl;
+       cin >> n;
+
+       while (n< 10 || n > 30) {
+           cout<<"Ingresar lo que se pidio numero mayor a 10 o menor a 30: ";
+           cin >> n;
+       }
+       for (int i = 1; i <= n; i = i + 2) {
+           cout<<i<<endl;
+
+           return 0;
+
+           }*/
+
+
+/*    int a, i;
+    cout<<"Numero mayor que de 10 y menor que 20: ";
+    cin >>a;
+
+    while (a <= 10 || a >= 30) {
+        cout<<"\nIngresar lo que se pidio numero mayor a 10 o menor a 30: ";
+        cin>>a;
+    }
+    i = 1;
+    while (i <= a) {
+        cout<<i<<endl;
+        i += 2;
+
+        return 0;
+    }*/
+
+
+ int dia;
+     cout<<"Ingresar un numero entre el 1 y el 5: ";cin>>dia;
+     while(dia < 1 || dia > 5) {
+         cout<<"Mostar Numero del 1 al 5: ";
+         cin>>dia;
+     }
+     switch (dia) {
+         case 1: cout <<"\nDia Lunes.";break;
+         case 2: cout <<"\nDia Martes.";break;
+         case 3: cout <<"\nDia Miercoles.";break;
+         case 4: cout <<"\nDia Jueves.";break;
+         case 5: cout <<"\nDia viernes.";break;
+
+         return 0;
+}
+    {
+    }
+}
